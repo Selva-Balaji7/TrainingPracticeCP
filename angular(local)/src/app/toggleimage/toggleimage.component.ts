2af9ba4ca87b8e46c34cd34d410ec07c08684df8
Selva-Balaji7 <tharunbalaji1807@gmail.com>
2024-12-13 08:11:08 +0530

    modified

--- angular(local)/src/app/toggleimage/toggleimage.component.ts
+++ angular(local)/src/app/toggleimage/toggleimage.component.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-toggle-image',
-  imports: [FormsModule],
-  templateUrl: './toggle-image.component.html',
-  styleUrl: './toggle-image.component.css'
+  imports: [CommonModule],
+  templateUrl: './toggleimage.component.html',
+  styleUrl: './toggleimage.component.css'
 })
 export class ToggleImageComponent {
  imgPath1:string="./images/himalayas.jpg";
@@ -13,23 +14,33 @@ export class ToggleImageComponent {
  imgPath2:string="./images/nature.jpg";
  imgName2:string="nature"; 
 
- isFirstImage:boolean=true;
- 
+ isimgPath1: boolean = true;
+ isimgPath2: boolean = false;
 
- myStyle1={
-   display:this.isFirstImage ? 'block' : 'none'
- }
- myStyle2={
-   display:this.isFirstImage ? 'none' : 'block'
- }
- 
 
-ToggleImage(){
+   myStyle1={
   
-  this.isFirstImage=!this.isFirstImage;
-  
+      display: this.isimgPath1 ? 'block' : 'none'
+    }
 
+
+   myStyle2={
+      display: this.isimgPath2 ? 'block' : 'none'
+  
+  }
+
+  toggleImage() {
+    if(this.isimgPath1){
+      
+      this.myStyle1.display='none';
+      this.myStyle2.display='block';
+        
+     }
+    else
+     {
+      this.myStyle1.display='block';
+      this.myStyle2.display='none';
+
+     }
 }
-
-
 }
