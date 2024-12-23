5e6a7acdfa06e63590dc6cb2969c547914a7abc6
Selva-balaji7 <tharunbalaji1807@gmail.com>
2024-12-23 18:11:07 +0530

    .net files

---
+++ Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace dotnetturtorials
+{
+    class Program
+    {
+        int numid;
+         public static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+            Console.Write("Hello, World!");
+          
+            int id = 1;
+            string name = "selva balaji";
+            double height = 5.5;
+            bool isMarried = false;
+            Console.WriteLine(name + "id is" + id + "and height is:" + height);
+            Console.WriteLine("Marital Status:" + isMarried);
+            Console.ReadKey();
+           
+
+        }
+    
+    
+    }
+}
+
