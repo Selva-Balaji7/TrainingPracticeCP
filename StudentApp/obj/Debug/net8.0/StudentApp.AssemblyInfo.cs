fe1a75bca51489f3a7644d4c1f2efe8a7388ad1c
Selva-balaji7 <tharunbalaji1807@gmail.com>
2024-12-30 09:38:46 +0530

    added

--- StudentApp/obj/Debug/net8.0/StudentApp.AssemblyInfo.cs
+++ StudentApp/obj/Debug/net8.0/StudentApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a4d861c38b230f11def0c2001ad7d5b23b61de2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ea8efca839fd2c14992fb42fde09030c0cfc69")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
